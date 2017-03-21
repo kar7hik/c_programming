b0f6787d0f3904b35d6bd93e16d8a3ede3348bac
ka7thik <karthikkumar377@gmail.com>
2017-03-21 23:08:28 +0530

    new files added to the C17

--- C17/linked_list.c
+++ C17/linked_list.c
@@ -29,7 +29,83 @@ struct node *add_to_list(struct node *list, int n) {
 }
 
 
-int main(void) {
+struct node *read_numbers(void) {
+  struct node *first = NULL;
+  int n;
+
+  printf("Enter a series of integers (0 to terminate): ");
+
+  for (;;) {
+    scanf("%d", &n);
+
+    if (n == 0)
+      return first;
+    first = add_to_list(first, n);
+  }
+}
+
+struct node *search_list(struct node *list, int n) {
+  struct node *p;
+  /*
+  for (p = list; p != NULL; p = p->next)
+    if (p->value == n)
+      return p;
+  return NULL;
+  */
+
+  /*
+  for (; list != NULL; list = list->next)
+    if (list->value == n)
+      return list;
+  return NULL
+  */
+
+  /*
+  for (; list != NULL && list->value != n; list = list->next)
+    ;
+  return list;
+  */
+
+  while (list != NULL && list->value != n)
+    list = list->next;
+  return list;
   
+}
+
+
+struct node *delete_from_list(struct node *list, int n) {
+  struct node *cur, *prev;
+
+  for (cur = list, prev = NULL;
+       cur != NULL, cur->value != n;
+       prev = cur, cur = cur->next) {
+  }  // empty loop
+  
+  if (cur == NULL)
+    return list;   // n was not found
+
+  if (prev == NULL)
+    list = list->next;  // n is in the first node.
+  else
+    prev->next = cur->next;   // n is in some other node
+
+  free(cur);
+  
+  return list;
+}
+
+
+int main(void) {
+  struct node *first;
+  first = NULL;
+
+  read_numbers();
+  printf("enter the value to be found: ");
+  int value;
+  scanf("%d", &value);
+
+  printf("%p\n", search_list(first, 25));
+
   return 0;
 }
+
---
+++ C17/read_line.h
@@ -0,0 +1,9 @@
+
+// Copyright 2017 Karthik
+
+#ifndef LINE_H
+#define LINE_H
+
+int read_line(char str[], int n);
+
+#endif
